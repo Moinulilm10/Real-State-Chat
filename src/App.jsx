9f58674ca549d6c593fba9be0a4081665aa8135a
Moinulilm10 <moinulilm10@gmail.com>
2024-08-11 18:37:30 +0600

    feat: add home page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Navbar from "./components/common/Navbar";
+import HomePage from "./components/pages/HomePage";
 import "./style/layout.scss";
 
 function App() {
   return (
     <div className="layout">
       <Navbar />
+      <HomePage />
     </div>
   );
 }
