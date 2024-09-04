50e82e3344687bf5f5cc2bd5c5ed6468b894b77f
Moinulilm10 <moinulilm10@gmail.com>
2024-09-04 22:22:19 +0600

    feat: authorization add

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import authRoute from "./routes/auth.route.js";
+import testRoute from "./routes/test.route.js";
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
+app.use("/api/test", testRoute);
 
 app.listen(8000, () => {
   console.log("http://localhost:8000");
