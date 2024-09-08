113a8f63c924a064f102e1e9242a2b35d5d7482c
Moinulilm10 <moinulilm10@gmail.com>
2024-09-08 19:43:39 +0600

    feat: add user route, updated user controller, and modified prisma schema

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import express from "express";
 import authRoute from "./routes/auth.route.js";
 import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/test", testRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8000, () => {
   console.log("http://localhost:8000");
